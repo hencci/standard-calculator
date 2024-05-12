fdb6906967ba1947e0b090ec9db688816e5435d9
hencci <hencci20@gmail.com>
2024-05-12 11:41:13 +0100

    Created a fucntion that takes and operator and 2 numbers

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let num1;
+let mum2;
+let operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +16,17 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2); break;
+        case "-":
+            subtract(num1, num2); break
+        case "*":
+            multiply(num1, num2); break;
+        case "/":
+            divide(num1, num2); break;
+    }
 }
